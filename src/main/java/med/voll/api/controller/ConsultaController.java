32d1c1da5e06fabf085385af5658063cc828a912
NachoMansilla <knjasu098@gmail.com>
2024-05-15 18:12:43 -0400

    Se agrego el manejo del metodo delete para la funcionalidad de cancelamiento de consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,21 +3,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.domain.consulta.AgendaDeConsultaService;
-import med.voll.api.domain.consulta.DatosAgendarConsulta;
-import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import med.voll.api.domain.consulta.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultas")
 public class ConsultaController {
     @Autowired
     private AgendaDeConsultaService service;
+    private ConsultaRepository consultaRepository;
 
     @PostMapping
     @Transactional
@@ -27,6 +23,14 @@ public class ConsultaController {
         var response = service.agendar(datos);
 
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelarConsulta(@RequestBody @Valid DatosCancelamientoConsulta datosCancelamiento){
+        service.cancelar(datosCancelamiento);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
